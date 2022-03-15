923de3e8fcfc16c70dec38f856811b09634bbeb1
travis flake <tflake83@gmail.com>
2022-03-15 10:19:41 -0400

    revamp

---
+++ postgres.sql
@@ -0,0 +1,20 @@
+-- CREATE FUNCTION trigger_function() 
+--    RETURNS TRIGGER 
+--    LANGUAGE PLPGSQL
+-- AS $$
+-- BEGIN
+--     -- trigger logic
+--     END;
+-- $$
+INSERT INTO registration
+    (
+    first_name,
+    last_name,
+    email
+    )
+VALUES(
+        travsi,
+        jkasbdnv,
+        hudvudv@gmail.com
+);
+    

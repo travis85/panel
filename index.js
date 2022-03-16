65957d5f8cf9e10448bc232c98ceb62710b7cfca
travis flake <tflake83@gmail.com>
2022-03-16 13:08:44 -0400

    app pushed

---
+++ index.js
@@ -0,0 +1,37 @@
+const { Client } = require("pg");
+
+const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'Trav1612',
+    database: 'panel'
+})
+
+client.connect();
+client.query(`select * from registration`, (err, result) => {
+    if(!err){
+        console.log(result.rows);
+    } else{
+        console.log(err.message);
+
+    }
+    client.end();
+})
+
+
+
+// $(document).ready(function(){
+
+
+//     // var hiddenBox = $( "#submit" );
+//     $( "#submit" ).on( "click", function (event){
+            
+        
+//     });
+
+   
+
+// })
+
+
